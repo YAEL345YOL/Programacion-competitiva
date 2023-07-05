0ebefd1c7336ef220f0f0ba730762490e2116fd3
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-07-04 19:29:33 -0600

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -9,7 +9,6 @@ using namespace std;
 #define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
 #define number(x) x>=48 && x<=57 ? true:false
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0),cout.tie(0);
     return 0;
 }
