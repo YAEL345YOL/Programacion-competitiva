42e2cc174ff0fb16d35baed753a7fa4043a3dc30
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-08-13 14:45:36 -0600

    Update 7789. Hackers.cpp

--- Pendiente/7789. Hackers.cpp
+++ Pendiente/7789. Hackers.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define nl "\n"
-#define cnl cout<<"\n"
+#define cnl cout << "\n"
 #define ll long long
 #define ull unsigned long long
-#define sp(x) fixed<<setprecision(x)
-#define all(x) x.begin(),x.end()
-#define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
-#define number(x) x>=48 && x<=57 ? true:false
-int invert(string x){
-    string aux;
-    for(int y=x.size()-1;y>=0;y--){
-        if(x[y]!='0') aux+=x[y];
+#define sp(x) fixed << setprecision(x)
+#define all(x) x.begin(), x.end()
+#define letter(x) x >= 65 && x <= 90 || x >= 97 && x <= 122 ? true : false
+#define number(x) x >= 48 && x <= 57 ? true : false
+int codificar(int numero){
+    int k = 0;
+    string aux = to_string(numero),result;
+    for(int x=aux.size()-1;x>=0;x--){
+        if(aux[x]=='0' && k==0){
+            while(aux[x]=='0'){
+                x--;
+            }
+            k++;
+        }
+        result += aux[x];
     }
-    return stoi(aux);
+    return stoi(result);
 }
 int main(){
     cin.tie(0)->sync_with_stdio(0),cout.tie(0);
-    int n;
-    string n1,n2;
+    vector<int> v;
+    int n,aux1,aux2;
     cin>>n;
-    int nu[n]={0};
     for(int x=0;x<n;x++){
-        cin>>n1>>n2;
-        nu[x] = invert(to_string(invert(n1)+invert(n2)));
+        cin>>aux1>>aux2;
+        v.push_back(codificar(codificar(aux1) + codificar(aux2)));
     }
-    for(int x:nu){
-        cout<<x<<nl;
+    for(int aux:v){
+        cout<<aux<<nl;
     }
     return 0;
 }
