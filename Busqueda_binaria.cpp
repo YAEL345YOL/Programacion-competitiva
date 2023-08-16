8bdc73fb7661d974b75f1de15fb256f04dbc20f2
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-08-16 09:34:28 -0600

    Create Busqueda_binaria.cpp

---
+++ Busqueda_binaria.cpp
@@ -0,0 +1,10 @@
+bool busquedaBinaria(vector<int>&vec,int num){
+    int ini=0,fin=vec.size()-1,medio;
+    while(ini<=fin){
+        medio = (ini+fin)/2;
+        if(num==vec[medio]) return 1;
+        else if(num>vec[medio]) ini=medio+1;
+        else if(num<vec[medio]) fin=medio-1;
+    }
+    return 0;
+}
