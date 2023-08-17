02c4e680246e8ebbac99229390afc2dc8ced96a6
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-08-16 22:16:30 -0600

    Update and rename Busqueda_binaria.cpp to F Busqueda_binaria.cpp

--- F Busqueda_binaria.cpp
+++ F Busqueda_binaria.cpp
@@ -1,10 +1,10 @@
-bool busquedaBinaria(vector<int>&vec,int num){
+int busquedaBinaria(vector<int>&vec,int num){
     int ini=0,fin=vec.size()-1,medio;
     while(ini<=fin){
         medio = (ini+fin)/2;
-        if(num==vec[medio]) return 1;
+        if(num==vec[medio]) return medio;
         else if(num>vec[medio]) ini=medio+1;
         else if(num<vec[medio]) fin=medio-1;
     }
-    return 0;
+    return -1;
 }
