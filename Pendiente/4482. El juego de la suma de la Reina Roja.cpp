df6f31196db77967c5de0987483092966b06ef8b
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-08-24 16:10:06 -0600

    Update 4482. El juego de la suma de la Reina Roja.cpp

--- Pendiente/4482. El juego de la suma de la Reina Roja.cpp
+++ Pendiente/4482. El juego de la suma de la Reina Roja.cpp
@@ -1,7 +1,7 @@
+// YAEL345YOL
 #include <bits/stdc++.h>
 using namespace std;
 #define nl "\n"
-#define cnl cout<<"\n"
 #define ll long long
 #define ull unsigned long long
 #define sp(x) fixed<<setprecision(x)
@@ -9,27 +9,44 @@ using namespace std;
 #define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
 #define number(x) x>=48 && x<=57 ? true:false
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int numeroGuardias,numeroDeGritos;
-    ll aux;
-    cin>>numeroGuardias;
-    ll guardias[numeroGuardias];
-    for(int x=0;x<numeroGuardias;x++){
-        cin>>guardias[x];
-    }
-    cin>>numeroDeGritos;
-    ll guardiasParados[numeroDeGritos]={0};
-    for(int x=0;x<numeroDeGritos;x++){
-        cin>>aux;
-        for(int y=0;y<numeroGuardias;y++){
-            if(guardias[y]+aux==y+1){
-                guardiasParados[x]++;
+    cin.tie(0)->sync_with_stdio(0),cout.tie(0);
+    deque<ull>puntajes;
+    vector<string>acciones;
+    ull numeroAcciones;
+    cin>>numeroAcciones;
+    for(ull x=0;x<numeroAcciones;++x){
+        char accion;
+        cin>>accion;
+        if(accion=='R'){
+            ull puntaje;
+            cin>>puntaje;
+            puntajes.push_back(puntaje);
+        }
+        else if(accion=='S'){
+            string numbersConcat;
+            if(puntajes.size()<3){
+                acciones.push_back("NO HAY SUFICIENTES PUNTAJES");
+                continue;
             }
+            sort(all(puntajes));
+            for(ull y=0;y<3;++y){
+                numbersConcat+=to_string(puntajes[y]);
+                if(y!=2) numbersConcat+=' ';
+            }
+            acciones.push_back(numbersConcat);
+        }
+        else if(accion=='B'){
+            if(puntajes.empty()) continue;
+            sort(all(puntajes));
+            puntajes.pop_front();
         }
     }
-    for(int x:guardiasParados){
-        cout<<x<<' ';
+    for(string aux:acciones) cout<<aux<<nl;
+    if(puntajes.empty()){
+        cout<<"NO HUBO GANADOR";
+        return 0;
     }
+    sort(all(puntajes));
+    cout<<"EL PUNTAJE MAS ALTO FUE "<<puntajes[puntajes.size()-1];
     return 0;
 }
