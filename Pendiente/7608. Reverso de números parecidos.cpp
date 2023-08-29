603b8a9e2e158a4a04fad5dd025ce5bc8ff260ab
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-08-29 14:28:11 -0600

    Update 7608. Reverso de números parecidos.cpp

--- Pendiente/7608. Reverso de números parecidos.cpp
+++ Pendiente/7608. Reverso de números parecidos.cpp
@@ -2,13 +2,14 @@
 #include <stdio.h>
 using namespace std;
 int main(){
-    char* entrada,salida;
+    char entrada[8];
     int n;
     scanf("%d",&n);
+    char* arreglo[n];
     for(int x=0;x<n;++x){
         scanf("%s",entrada);
-        salida+='.';
-        salida=strcat(salida,entrada);
+        arreglo[x] = entrada;
     }
+    printf("%s",arreglo[3]);
     return 0;
 }
