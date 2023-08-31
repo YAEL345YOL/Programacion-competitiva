13ee6138691d62a3827d09e5fdc064b8d468054b
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-08-31 16:04:56 -0600

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -8,6 +8,10 @@ using namespace std;
 #define all(x) x.begin(),x.end()
 #define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
 #define number(x) x>=48 && x<=57 ? true:false
+// Valores del código ascii:
+// 'A' = 65 'Z' = 90
+// 'a' = 97 'z' = 122
+// '0' = 48 '9'
 int main(){
     cin.tie(0)->sync_with_stdio(0),cout.tie(0);
     return 0;
