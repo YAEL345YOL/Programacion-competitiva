682ee579aded7e65876f76c2f8b0f88db4065b95
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-09-28 18:30:58 -0600

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -2,15 +2,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define nl "\n"
+#define f first
+#define s second
+#define pb push_back
 #define ll long long
 #define ull unsigned long long
 #define sp(x) fixed<<setprecision(x)
 #define all(x) x.begin(),x.end()
+#define fore(it,i,f) for(auto it=i;it<f;++it)
 #define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
 #define number(x) x>=48 && x<=57 ? true:false
-// 'A' = 65 'Z' = 90
-// 'a' = 97 'z' = 122
-// '0' = 48 '9' = 57
+/*'A' = 65 'Z' = 90
+  'a' = 97 'z' = 122
+  '0' = 48 '9' = 57 */
 int main(){
     cin.tie(0)->sync_with_stdio(0),cout.tie(0);
     return 0;
