ca25fed071d6df1b40b85f58fa1fd25bcb433a48
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-11-07 12:32:40 -0600

    Update 192. L-OMI98-Submarino.cpp

--- Pendiente/192. L-OMI98-Submarino.cpp
+++ Pendiente/192. L-OMI98-Submarino.cpp
@@ -15,57 +15,61 @@ using namespace std;
 /*'A' = 65 'Z' = 90
   'a' = 97 'z' = 122
   '0' = 48 '9' = 57 */
-struct nodo{
-    ll number,steps=0;
-    bool visited=false;
-    bool operator<(const nodo&n2) const {
-        return number<n2.number;
-    }
-};
-ll BFS(vector<vector<nodo>>graph,nodo nodoInicial,nodo nodoFinal){
-    queue<nodo>visiting;
-    nodoInicial.visited=true;
-    visiting.push(nodoInicial);
-    while(!visiting.empty()){
-        nodo current = visiting.front();
-        visiting.pop();
-        if(current.number==nodoFinal.number) return current.steps;
-        for(auto&vecino:graph[current.number]){
-            if(!vecino.visited){
-                vecino.steps=current.steps+1;
-                vecino.visited=true;
-                visiting.push(vecino);
+vector<set<ll>>grafo;
+ll BFS(const ll&nodoInicial,const ll&nodoFinal){
+    vector<set<ll>>copiaGrafo = grafo;
+    
+    queue<ll>visitando;
+    vector<bool>visitado(copiaGrafo.size(),false);
+    vector<ll>distancias(copiaGrafo.size(),0);
+    
+    visitado[nodoInicial] = true; 
+    visitando.push(nodoInicial);
+    
+    while(!visitando.empty()){
+        ll actual = visitando.front();
+        visitando.pop();
+        
+        if(actual==nodoFinal) return distancias[actual];
+        
+        for(auto&vecino:copiaGrafo[actual]){
+            if(!visitado[vecino]){
+                distancias[vecino]=distancias[actual]+1;
+                visitado[vecino]=true;
+                visitando.push(vecino);
             }
         }
     }
+    
     return -1;
 }
-void addConection(vector<vector<nodo>>&graph,nodo&n1,nodo&n2){
-    graph[n1.number].pb(n2);
-    graph[n2.number].pb(n1);
+void agregarConeccion(const ll&nodo1,const ll&nodo2){
+    grafo[nodo1].insert(nodo2);
+    grafo[nodo2].insert(nodo1);
 }
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
-    ll numberOfNodes,numberOfConextions,minTime,trip=0;
-    cin>>numberOfNodes>>numberOfConextions>>minTime;
-    vector<vector<nodo>>graph(numberOfNodes+1);
-    set<nodo>nodos;
-    nodo nodoFinal;
-    nodoFinal.number=numberOfNodes;
+    ll n,e,t,cont=0;
+    cin>>n>>e>>t;
     
-    fore(i,0,numberOfConextions){
-        nodo auxiliary1,auxiliary2;
-        cin>>auxiliary1.number>>auxiliary2.number;
-        nodos.insert(auxiliary1);
-        nodos.insert(auxiliary2);
-        addConection(graph,auxiliary1,auxiliary2);
+    grafo.resize(n+1);
+    
+    fore(i,0,e){
+        ll n1,n2;
+        cin>>n1>>n2;
+        agregarConeccion(n1,n2);
     }
     
-    for(nodo n:nodos) if(BFS(graph,n,nodoFinal)<minTime && n.number!=numberOfNodes) ++trip;
+    /*for(auto&a:grafo){
+        for(auto&b:a) cout<<b<<' '; 
+        cout<<nl;
+    }*/
     
-    cout<<trip;
+    fore(i,1,n) if(BFS(i,n)<t && (!grafo[i].empty())) ++cont;
     
+    cout<<cont;
+  
     return 0;
 }
