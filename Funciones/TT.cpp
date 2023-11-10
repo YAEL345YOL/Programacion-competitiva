8fa9f140f154236c76aa08065496c17063ef52db
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-11-10 09:47:47 -0600

    Update TT.cpp

--- Funciones/TT.cpp
+++ Funciones/TT.cpp
@@ -23,11 +23,36 @@ struct nodo{
     }
 };
 vector<set<pair<nodo*,ll>>>matriz(4);
+void addConection(nodo*n1,nodo*n2, const ll&peso){
+    matriz[n1->num].insert({n2,peso});
+    matriz[n2->num].insert({n1,peso});
+}
+void Djikstra(nodo*nodoInicial){
+    priority_queue<nodo*,vector<nodo*>,greater<nodo*>>pq;
+    nodoInicial.distancia = 0;
+    pq.push(nodoInicial);
+    while(!pq.empty()){
+        nodo* act = pq.top();
+        pq.pop();
+        for(auto&vecino:matriz[act.num]){
+        }
+    }
+}
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
+    ll n;
+    cin>>n;
     
+    fore(i,0,n){
+        ll peso;
+        nodo *n1,*n2;
+        n1 = new nodo;
+        n2 = new nodo;
+        cin>>n1->num>>n2->num>>peso;
+        addConection(n1,n2,peso);
+    }
     
     return 0;
 }
