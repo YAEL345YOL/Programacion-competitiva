6519338d5fc6f19ac744d6aeebd9ff5bc7a2e000
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-11-11 22:39:15 -0600

    Update Posición valida matriz.cpp

--- Funciones/Posición valida matriz.cpp
+++ Funciones/Posición valida matriz.cpp
@@ -1,3 +1,3 @@
-bool valid(const int&fila,const int&columna,vector<vector<int>>&matriz){
-    return fila>=0 and columna>=0 and fila<matriz.size() and columna<matriz[0].size() and matriz[fila][columna]==1;
+bool valid(const ll&fila,const ll&columna,vector<vector<ll>>&matriz){
+    return fila>=0 && columna>=0 && fila<matriz.size() && columna<matriz[0].size();
 }
