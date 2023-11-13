46a67085253a1a1f846aa74710c6a89b0184b88f
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-11-13 10:25:02 -0600

    Create J. La banda robatamales.cpp

---
+++ Compartir/J. La banda robatamales.cpp
@@ -0,0 +1,30 @@
+// ΨΔΣL345ΨΘL
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define f first
+#define s second
+#define pb push_back
+#define ll long long
+#define ull unsigned long long
+#define sp(x) fixed<<setprecision(x)
+#define all(x) x.begin(),x.end()
+#define fore(it,i,f) for(auto it=i;it<f;++it)
+#define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
+#define number(x) x>=48 && x<=57 ? true:false
+/*'A' = 65 'Z' = 90
+  'a' = 97 'z' = 122
+  '0' = 48 '9' = 57 */
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll tamalesRobados,cantidadDeIntegrantes;
+    cin>>tamalesRobados>>cantidadDeIntegrantes;
+    
+    --cantidadDeIntegrantes;
+    
+    cout<<(tamalesRobados/2 + tamalesRobados%2) + ((tamalesRobados-(tamalesRobados/2 + tamalesRobados%2))%cantidadDeIntegrantes);
+  
+    return 0;
+}
