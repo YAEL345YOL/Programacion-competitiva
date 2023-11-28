63c06e4256b11db579ffa015878d875d2b081d78
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2023-11-27 23:18:23 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 // ΨΔΣL345ΨΘL
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
 #include <bits/stdc++.h>
 using namespace std;
 #define nl "\n"
 #define f first
 #define s second
 #define pb push_back
+#define ld long double
 #define ll long long
 #define ull unsigned long long
 #define sp(x) fixed<<setprecision(x)
@@ -12,13 +14,17 @@ using namespace std;
 #define fore(it,i,f) for(auto it=i;it<f;++it)
 #define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
 #define number(x) x>=48 && x<=57 ? true:false
+#define inputF() freopen("input.txt","r",stdin);
+#define outputF() freopen("output.txt","w",stdout);
 /*'A' = 65 'Z' = 90
   'a' = 97 'z' = 122
-  '0' = 48 '9' = 57 */
+  '0' = 48 '9' = 57 
+*/
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
+    // Code
   
     return 0;
 }
