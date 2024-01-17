36d0a3f4021b2357a1e4ad4c98a09fa81794f203
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2024-01-17 15:56:50 -0600

    Update Posición valida matriz.cpp

--- Funciones/Posición valida matriz.cpp
+++ Funciones/Posición valida matriz.cpp
@@ -1,3 +1,10 @@
+/*
+N = filas de la matriz
+M = columnas de la matriz
+f = fila a validar
+c = columna a validar
+*/
+
 bool posValid(const ll&N,const ll&M,const ll&f,const ll&c){
     return f>=0 && c>=0 && f<N && c<M; 
 }
