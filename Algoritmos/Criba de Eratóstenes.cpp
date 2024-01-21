8d06408f18bf5aa54418a8306b04b47fa4e2b153
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2024-01-20 19:49:16 -0600

    Create Criba de Eratóstenes.cpp

---
+++ Algoritmos/Criba de Eratóstenes.cpp
@@ -0,0 +1,9 @@
+vector<bool>cribaDeEratostenes(1000000,1);
+void generarPrimos(){
+    cribaDeEratostenes[0] = cribaDeEratostenes[1] = 0;
+    for(ll i=2;i*i<=cribaDeEratostenes.size();++i){
+        if(cribaDeEratostenes[i]){
+            for(ll j=i*i;j<cribaDeEratostenes.size();j+=i) cribaDeEratostenes[j] = 0;
+        }
+    }
+}
