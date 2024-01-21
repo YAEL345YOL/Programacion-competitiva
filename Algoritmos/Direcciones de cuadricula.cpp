f5127c945edb56c97179e46aba0e79197dfe59cc
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2024-01-20 19:57:09 -0600

    Update Direcciones de cuadricula.cpp

--- Algoritmos/Direcciones de cuadricula.cpp
+++ Algoritmos/Direcciones de cuadricula.cpp
@@ -1,6 +1,8 @@
-// Cuadro casillas (arriba, abajo, derecha y izquierda)
+// Determina si una posición a la que se quiere ingresar es valid en relacion NxM.
+bool posValid(const ll&N,const ll&M,const ll&fi,const ll&co){ return fi>=0 && co>=0 && fi<N && co<M; }
+// Cuadro casillas (arriba, abajo, derecha y izquierda).
 int dr[] = {-1,0,1,0};
 int dc[] = {0,1,0,-1};
-// Ocho casillas
+// Ocho casillas.
 int dr[] = {-1,-1,0,1,1,1,0,-1},
 int dc[] = {0,-1,-1,-1,0,1,1,1};
