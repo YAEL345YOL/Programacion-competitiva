9ad8ea8f2a96c0e3123c801c8d5058d8a597d5ab
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2024-01-20 19:11:49 -0600

    Update Suma de dos cadenas.cpp

--- Algoritmos/Suma de dos cadenas.cpp
+++ Algoritmos/Suma de dos cadenas.cpp
@@ -1,42 +1,13 @@
-// ΨΔΣL345ΨΘL
-#include <bits/stdc++.h>
-using namespace std;
-#define nl "\n"
-#define f first
-#define s second
-#define pb push_back
-#define ld long double
-#define ll long long
-#define ull unsigned long long
-#define sp(x) fixed<<setprecision(x)
-#define all(x) x.begin(),x.end()
-#define fore(it,i,f) for(auto it=i;it<f;++it)
-#define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
-#define number(x) x>=48 && x<=57 ? true:false
-/*'A' = 65 'Z' = 90
-  'a' = 97 'z' = 122
-  '0' = 48 '9' = 57 */
-ll sumaDosCadenas(string&n1,string&n2){
-    string resultado = "";
-    ll i = n1.size()-1,j = n2.size()-1, carreo=0, carreos=0;
+// Suma resultante de sumar dos cadenas de numeros (funcional para numeros gigantescos).
+string sumaDosNumerosCadenas(const string&s1,const string&s2){
+    string sumaFinal = "";
+    ll i = s1.size()-1,j = s2.size()-1, carreo=0;
     while(i>=0 or j>=0 or carreo>0){
-        ll suma = (i>=0 ? n1[i]-48:0) + (j>=0 ? n2[j]-48:0) + carreo;
+        ll suma = (i>=0 ? s1[i]-48:0) + (j>=0 ? s2[j]-48:0) + carreo;
         carreo = suma / 10;
-        if(carreo!=0) ++carreos;
-        resultado.insert(resultado.begin(),(char)(suma%10)+48);
+        sumaFinal.insert(sumaFinal.begin(),(suma%10)+'0');
         --i;
         --j;
     }
-    return carreos;
-}
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    string n1,n2;
-    cin>>n1>>n2;
-
-    cout<<sumaDosCadenas(n1,n2);
-
-    return 0;
+    return sumaFinal;
 }
