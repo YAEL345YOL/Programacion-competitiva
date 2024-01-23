21d031833eda88c55980bf8af6205464fd67d2f4
DevYangz <50465405+YAEL345YOL@users.noreply.github.com>
2024-01-22 18:12:29 -0600

    Create Las bitácoras de la sana distancia.cpp

---
+++ Pendiente/Las bitácoras de la sana distancia.cpp
@@ -0,0 +1,54 @@
+// ΨΔΣL345ΨΘL
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define f first
+#define s second
+#define pb push_back
+#define ld long double
+#define ll long long
+#define ull unsigned long long
+#define sp(x) fixed<<setprecision(x)
+#define all(x) x.begin(),x.end()
+#define fore(it,i,f) for(auto it=i;it<f;++it)
+#define letter(x) x>=65 && x<=90 || x>=97 && x<=122 ? true:false
+#define number(x) x>=48 && x<=57 ? true:false
+/*'A' = 65 'Z' = 90
+  'a' = 97 'z' = 122
+  '0' = 48 '9' = 57 */
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n,suma=0;
+    cin>>n;
+    
+    queue<ll>duraciones;
+    queue<string>numeros;
+    stack<string>atendiendo;
+    
+    fore(i,0,n){
+        ll aux;
+        cin>>aux;
+        suma+=aux;
+        duraciones.push(suma);
+    }
+    
+    fore(i,0,n){
+        string numero;
+        cin>>numero;
+        numeros.push(numero);
+    }
+    
+    cout<< numeros.front() << nl;
+    
+    numeros.pop();
+    
+    ll contadorMinutos=1;
+    
+    while(!numeros.empty()){
+        
+    }
+    
+    return 0;
+}
