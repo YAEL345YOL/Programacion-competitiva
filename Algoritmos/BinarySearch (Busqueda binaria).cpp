7750ac525965a66e33631c828b17bfbe02a77593
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-02-03 22:35:34 -0600

    Update BinarySearch (Busqueda binaria).cpp

--- Algoritmos/BinarySearch (Busqueda binaria).cpp
+++ Algoritmos/BinarySearch (Busqueda binaria).cpp
@@ -1,10 +1,10 @@
-ll binarySearch(vector<ll>&vec,const ll&num){
-    ll left=0,right=vec.size()-1,mid;
-    while(left<=right){
-        mid = (left+right)/2;
-        if(num==vec[mid]) return mid;
-        else if(num>vec[mid]) left=mid+1;
-        else if(num<vec[mid]) right=mid-1;
+ll binarySearch(vector<ll>&vec,const ll&numero){
+    ll inicio=0,fin=vec.size()-1,medio;
+    while(inicio<=fin){
+        medio = (inicio+fin)/2;
+        if(numero==vec[medio]) return medio;
+        else if(numero>vec[medio]) inicio=medio+1;
+        else if(numero<vec[medio]) fin=medio-1;
     }
     return -1;
 }
