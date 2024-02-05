7e1a286d1f7fbab7dd0ad5c30525c6c19bd9327a
YAEL345YOL <lenin346yael@gmail.com>
2024-02-05 15:18:44 -0600

    MCD

---
+++ Codigos/MCD.cpp
@@ -0,0 +1,11 @@
+// Algoritmo de euclides O(log(min(a, b)));
+ll algoritmoEuclides(ll a,ll b){
+    if(b>a) swap(a,b);
+    while(a%b!=0){
+        ll aux;
+        aux = a;
+        a = b;
+        b = aux%b;
+    }   
+    return b;
+}
