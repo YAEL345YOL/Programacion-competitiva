cc06eb00169bca490d332d4f62fa8e5642cec350
YAEL345YOL <lenin346yael@gmail.com>
2024-02-04 19:14:52 -0600

    idk

---
+++ Codigos/Exponenciación Binaria.cpp
@@ -0,0 +1,12 @@
+unordered_map<ull,ull>mem;
+ull fpow(ull n,ull pot){
+    if(mem.count(pot)>0) return mem[pot];
+
+    if(pot==0) return 1;
+    if(pot==1) return n;
+
+    if(pot%2==0) mem[n] = fpow(n,pot/2);
+    else mem[n] = fpow(n,pot-1)*n;
+
+    return mem[n];
+}
