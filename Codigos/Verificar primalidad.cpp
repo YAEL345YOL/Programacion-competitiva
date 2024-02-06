97380dbb1ab826a1ed97e905c3e85d0343647c84
YAEL345YOL <lenin346yael@gmail.com>
2024-02-06 16:13:09 -0600

    O(sqrt(n)) --

--- Codigos/Verificar primalidad.cpp
+++ Codigos/Verificar primalidad.cpp
@@ -1,4 +1,9 @@
 bool primo(const ll n){
-    for(ll i=2;i*i<=n;++i) if(n%i==0) return 0;
+    if(n==2 or n==3) return 1;
+    if(n<=1 or n%2==0 or n%3==0) return 0;
+    for(long i=5;i*i<=n;i+=6)
+        if(n%i==0 || n%(i+2)==0) 
+            return 0;
+    
     return 1;
 }
