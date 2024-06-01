504d5562718966c7150ba73eec0ad1079fb303fd
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-01 08:26:28 -0600

    Update Criba de Eratóstenes.cpp

--- Codigos/Number theory/Criba de Eratóstenes.cpp
+++ Codigos/Number theory/Criba de Eratóstenes.cpp
@@ -2,8 +2,8 @@ ll MAX = 10000000;
 vb prime(MAX,1);
 void sieve(){
     prime[0] = prime[1] = 0;
-    for(auto i=2;i*i<=MAX;++i)
+    for(ll i=2;i*i<=MAX;++i)
         if(prime[i])
-            for(auto j=i*i;j<=MAX;j+=i)
+            for(ll j=i*i;j<=MAX;j+=i)
                 prime[j] = 0;
 }
