a7fa32f58f5780a64e78fd6d488c868b7325b32b
YAEL345YOL <lenin346yael@gmail.com>
2024-06-02 00:22:03 -0600

    modified:   Codigos/DS/Segment Tree.cpp

--- Codigos/DS/Segment Tree.cpp
+++ Codigos/DS/Segment Tree.cpp
@@ -1,27 +1,30 @@
 struct segmentTree{
     vll segtree;
-    segmentTree(ll n):segtree(2*n-1){}
+    segmentTree(ll n):segtree(4*n){}
     void build(ll l,ll r,ll n,vll arr){
         if(l==r){
             segtree[n] = arr[l];
             return;
         }
-        build(l,(l+r)/2,2*n+1,arr); 
-        build((l+r)/2+1,r,2*n+2,arr); 
-        segtree[n] = max(segtree[2*n+1],segtree[2*n+2]);
+        ll m = (l+r)/2;
+        build(l,m,2*n,arr); 
+        build(m+1,r,2*n+1,arr); 
+        segtree[n] = max(segtree[2*n],segtree[2*n+1]);
     }
     void update(ll l,ll r,ll n,ll p,ll nv){
         if(l==r){
             segtree[n] = nv;
             return;
         }
-        if(p<=(l+r)/2) update(l,(l+r)/2,2*n+1,p,nv);
-        else update((l+r)/2+1,r,2*n+2,p,nv);
-        segtree[n] = max(segtree[2*n+1],segtree[2*n+2]);
+        ll m = (l+r)/2;
+        if(p<=m) update(l,m,2*n,p,nv);
+        else update(m+1,r,2*n+1,p,nv);
+        segtree[n] = max(segtree[2*n],segtree[2*n+1]);
     }
     ll query(ll l,ll r,ll n,ll i,ll j){
+        if(l>j or r<i) return INT32_MIN;
         if(l>=i && r<=j) return segtree[n];
-        else if(l>j or r<i) return INT32_MIN;
-        return max(query(l,(l+r)/2,2*n+1,i,j),query((l+r)/2+1,r,2*n+2,i,j));
+        ll m = (l+r)/2;
+        return max(query(l,m,2*n,i,j),query(m+1,r,2*n+1,i,j));
     }
 };
