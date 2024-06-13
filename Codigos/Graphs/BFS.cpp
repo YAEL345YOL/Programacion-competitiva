747234d913c8ad1ddd5c5e3400630791c8827ec9
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-13 17:01:29 -0600

    Update BFS.cpp

--- Codigos/Graphs/BFS.cpp
+++ Codigos/Graphs/BFS.cpp
@@ -1,18 +1,16 @@
 vvll graph;
 vll dist;
 
-void BFS(ll f){
-    vb visited(graph.size(),0);
+void BFS(ll S){
     queue<ll>visiting;
-    visited[f] = 1;
-    visiting.push(f);
+    dist[S] = 0;
+    visiting.push(S);
     while(!visiting.empty()){
         ll u = visiting.front();
         visiting.pop();
         cout<<u<<nl;
         for(auto&v:graph[u]){
-            if(!visited[v]){
-                visited[v] = 1;
+            if(dist[v]==-1){
                 dist[v] = dist[u] + 1;
                 visiting.push(v);
             }
