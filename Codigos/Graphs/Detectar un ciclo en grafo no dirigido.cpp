5263f5f1af864cb9bd2961d9c6dacb6c84f8067e
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-17 05:59:20 -0600

    Update Detectar un ciclo en grafo no dirigido.cpp

--- Codigos/Graphs/Detectar un ciclo en grafo no dirigido.cpp
+++ Codigos/Graphs/Detectar un ciclo en grafo no dirigido.cpp
@@ -1,10 +1,17 @@
 vvll graph;
 vll visited;
 
-bool detectCycleUndirectedGraph(ll u,ll parent){
+bool detectCycleComponent(ll u,ll parent){
     visited[u] = 1;
     for(auto&v:graph[u])
-        if((visited[v] && v!=parent) || (!visited[v] && detectCycleUndirectedGraph(v,u))) 
+        if((visited[v] && v!=parent) || (!visited[v] && detectCycleComponent(v,u))) 
+            return 1;
+    return 0;
+}
+
+bool detectCycleUndirectedGraph(){
+    fore(i,0,n)
+        if(!visited[i] && detectCycleComponent(i,-1));
             return 1;
     return 0;
 }
