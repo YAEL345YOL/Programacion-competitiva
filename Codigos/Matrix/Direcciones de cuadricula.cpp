1977d2ab758c3b598e2401a29f2e444027ac5fab
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-17 22:29:03 -0600

    Update Direcciones de cuadricula.cpp

--- Codigos/Matrix/Direcciones de cuadricula.cpp
+++ Codigos/Matrix/Direcciones de cuadricula.cpp
@@ -1,8 +1,7 @@
-// Determina si una posición a la que se quiere ingresar es valid en relacion NxM.
 bool valid(par u){ return u.first>=0 && u.second>=0 && u.first<n && u.second<u.second; }
-// Cuadro casillas (arriba, abajo, derecha y izquierda).
-vll dx = {1,-1,0,0};
-vll dy = {0,0,1,-1};
-// Ocho casillas.
-int dr[] = {0, 0, 1, 1, 1, -1, -1, -1};
-int dc[] = {-1, 1, -1, 0, 1, -1, 0, 1};
+
+vll dx = {-1,0,0,1};
+vll dy = {0,-1,1,0};
+
+vll dx = {-1,-1,-1,0,0,1,1,1};
+vll dy = {-1,0,1,-1,1,-1,0,1};
