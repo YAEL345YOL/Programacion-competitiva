e43110bf80c803ce6f54350382e88fad188f6365
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-17 22:09:47 -0600

    Create Determinar si un grafo es bipartito.cpp

---
+++ Codigos/Graph/Determinar si un grafo es bipartito.cpp
@@ -0,0 +1,21 @@
+vvll graph;
+vll state;
+
+bool isBipartite(ll S){
+    state.resize(graph.size(),-1);
+    queue<ll>visiting;
+    state[S] = 0;
+    visiting.push(S);
+    while(!visiting.empty()){
+        ll u = visiting.front();
+        visiting.pop();
+        for(auto&v:graph[u]){
+            if(state[v]==state[u]) return 0;
+            if(state[v]==-1){
+                state[v] = 1 - state[u];
+                visiting.push(v);
+            }
+        }
+    }
+    return 1;
+}
