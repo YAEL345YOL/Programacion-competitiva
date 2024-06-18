8419b4a40e342f00c739f87812e90b1c414078f4
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-18 11:38:47 -0600

    Update Direcciones de cuadricula.cpp

--- Codigos/Matrix/Direcciones de cuadricula.cpp
+++ Codigos/Matrix/Direcciones de cuadricula.cpp
@@ -1,4 +1,4 @@
-bool valid(par u){ return u.first>=0 && u.second>=0 && u.first<n && u.second<u.second; }
+bool valid(par u){ return u.first>=0 && u.second>=0 && u.first<n && u.second<m; }
 
 vll dx = {-1,0,0,1};
 vll dy = {0,-1,1,0};
