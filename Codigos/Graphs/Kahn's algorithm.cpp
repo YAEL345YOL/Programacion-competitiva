c2604b423d1c4fd723811aaa1b0232fad476ac19
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-17 20:38:17 -0600

    Update Kahn's algorithm.cpp

--- Codigos/Graphs/Kahn's algorithm.cpp
+++ Codigos/Graphs/Kahn's algorithm.cpp
@@ -2,17 +2,17 @@ vvll graph;
 vll state;
 
 vll topologicalSort(){
-    for(auto&ady:graph) 
-        for(auto&u:ady) 
-            ++state[u];
-
+    state.resize(graph.size(),0);
     queue<ll>visiting;
+    vll res;
+
+    for(auto&u:graph) 
+        for(auto&v:u) 
+            ++state[v];
 
     fore(i,0,state.size())
         if(state[i]==0) 
             visiting.push(i);
-
-    vll res;
 
     while(!visiting.empty()){
         ll u = visiting.front();
@@ -22,11 +22,14 @@ vll topologicalSort(){
             state[v]-=1;
             if(state[v]==0)
                 visiting.push(v);
-            
         }
     }
     
-    if(res.size()!=state.size()) res.clear();
+    if(res.size()!=graph.size()) res.clear();
 
     return res;
 }
+/*
+    don't forget:
+    1. The graph needs to be a DAG (directed acyclic graph) for get n valid answer, otherwise the fuction will return am empty vector.
+*/
