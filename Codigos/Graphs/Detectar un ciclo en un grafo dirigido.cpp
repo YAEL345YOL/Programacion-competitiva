f74545836b4b7bfa354260e56f80b9433c864ebe
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-19 13:17:06 -0600

    Update Detectar un ciclo en un grafo dirigido.cpp

--- Codigos/Graphs/Detectar un ciclo en un grafo dirigido.cpp
+++ Codigos/Graphs/Detectar un ciclo en un grafo dirigido.cpp
@@ -1,19 +1,18 @@
 vvll graph;
-vll state;
 
-bool detectCycleComponent(ll u){
+bool detectCycleComponent(ll u,vll&state){
     state[u] = 1;
     for(auto&v:graph[u])
-        if(state[v]==1 || (state[v]==0 && detectCycleComponent(v))) 
+        if(state[v]==1 || (state[v]==0 && detectCycleComponent(v,state))) 
             return 1; 
     state[u] = 2;
     return 0;
 }
 
 bool detectCycleDirectedGraph(){
-    state.resize(graph.size(),0);
+    vll state(graph.size(),0);
     fore(i,0,graph.size())
-        if(state[i]==0 && detectCycleComponent(i))
+        if(state[i]==0 && detectCycleComponent(i,state))
             return 1;
     return 0;
 }
