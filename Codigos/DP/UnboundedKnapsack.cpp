888513e8119ee4667b7f196debd0169cac77f298
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-21 17:58:18 -0600

    Create UnboundedKnapsack.cpp

---
+++ Codigos/DP/UnboundedKnapsack.cpp
@@ -0,0 +1,13 @@
+ll UnboundedKnapsack(vpar&items,ll W){
+    vll dp(W+1,0);
+
+    fore(i,0,W+1){
+        fore(j,0,items.size()){
+            if(items[j].first<=i){
+                dp[i] = max(dp[i],dp[i-items[j].first]+items[j].second);
+            }
+        }
+    }
+
+    return dp[W];
+}
