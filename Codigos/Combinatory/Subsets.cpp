59454c5c70d087f5102da837fff869d421e5f325
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-21 18:33:26 -0600

    Update Subsets.cpp

--- Codigos/Combinatory/Subsets.cpp
+++ Codigos/Combinatory/Subsets.cpp
@@ -1,13 +1,13 @@
 vll arr,act;
 vvll subsets;
 
-void subsets(ll i){
+void subset(ll i){
     if(i==arr.size()){
         subsets.pb(act);
         return;
     }
     act.pb(arr[i]);
-    subsets(i+1);
+    subset(i+1);
     act.pop_back();
-    subsets(i+1);
+    subset(i+1);
 }
