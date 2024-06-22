3b57d990e30aa1ae3eaa20d6909453ff9477449f
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-21 18:58:04 -0600

    Update 1-0 Knapsack.cpp

--- Codigos/DP/1-0 Knapsack.cpp
+++ Codigos/DP/1-0 Knapsack.cpp
@@ -1,14 +1,18 @@
-ll Knapsack(vpar&items,ll W){
+vvll Knapsack(vpar&items,ll W){
     vvll dp(items.size()+1,vll(W+1,0));
 
     fore(i,1,dp.size()){
         fore(w,1,dp[0].size()){
-            if(items[i-1].first<=w) dp[i][w] = max(dp[i-1][w],dp[i-1][w-items[i-1].first]+items[i-1].second);
-            else dp[i][w] = dp[i-1][w];
+            if(items[i-1].first<=w){
+                dp[i][w] = max(dp[i-1][w],dp[i-1][w-items[i-1].first]+items[i-1].second);
+            }
+            else{
+                dp[i][w] = dp[i-1][w];
+            }
         }
     }   
 
-    return dp[items.size()][W];
+    return dp;
 }
 /*
     don't forget:
