9279a42940a9dbc91b01a7f794ff35f32caae2ca
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-21 18:30:09 -0600

    Update N-size combinations.cpp

--- Codigos/Combinatory/N-size combinations.cpp
+++ Codigos/Combinatory/N-size combinations.cpp
@@ -1,20 +1,16 @@
-vll act;
+vll arr, act;
+vvll combinations;
 
 void combination(ll i,ll s){
     if(act.size()==s){
-        for(auto&e:act) cout<<e<<' ';
-        cout<<nl;
+        combinations.pb(act);
         return;
     }
-
     if(i==arr.size()) return;
-
     act.pb(arr[i]);
     combination(i,s);
     act.pop_back();
-
     act.pb(arr[i]);
     combination(i+1,s);
     act.pop_back();
-
 }
