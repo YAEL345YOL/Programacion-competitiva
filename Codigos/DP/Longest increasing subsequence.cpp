c61f7e1557a856a02e9a474a33d92648381747c6
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-23 14:19:24 -0600

    Create Longest increasing subsequence.cpp

---
+++ Codigos/DP/Longest increasing subsequence.cpp
@@ -0,0 +1,15 @@
+vll arr;
+
+vll LCS(){
+    vll dp(arr.size(),1);
+
+    for(ll i=arr.size()-1;i>=0;--i){
+        for(ll j=i;j<arr.size();++j){
+            if(arr[i]<arr[j]){
+                dp[i] = max(dp[i],dp[j]+1);
+            }
+        }
+    }
+
+    return dp;
+}
