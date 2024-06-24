a9887c78b80997f4084edad15a18531ac5be762b
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-24 14:59:37 -0600

    Create Longest increasing subsequence binary search.cpp

---
+++ Codigos/DP/Longest increasing subsequence binary search.cpp
@@ -0,0 +1,19 @@
+vll arr;
+
+ll LISBS(){
+    vll dp;
+
+    dp.pb(arr[0]);
+
+    fore(i,1,arr.size()){
+        if(arr[i]>dp[dp.size()-1]){
+            dp.pb(arr[i]);
+        }
+        else{
+            auto it = lower_bound(all(dp),arr[i]);
+            dp[it-dp.begin()] = arr[i];
+        }
+    }
+
+    return dp.size();
+}
