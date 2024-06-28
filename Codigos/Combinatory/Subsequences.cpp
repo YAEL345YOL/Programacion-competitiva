4017b7e96832180155d36588b00f1f94ce6d5254
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2024-06-27 18:41:01 -0600

    Update and rename Subsets.cpp to Subsequences.cpp

--- Codigos/Combinatory/Subsequences.cpp
+++ Codigos/Combinatory/Subsequences.cpp
@@ -1,13 +1,13 @@
 vll arr, act;
 vvll subsets;
 
-void subset(ll i){
+void subsequence(ll i){
     if(i==arr.size()){
         subsets.pb(act);
         return;
     }
     act.pb(arr[i]);
-    subset(i+1);
+    subsequence(i+1);
     act.pop_back();
-    subset(i+1);
+    subsequence(i+1);
 }
