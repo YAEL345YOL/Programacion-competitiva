ec5925bb941e7f5f49db1210c61deff104209c6e
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2025-02-16 13:29:43 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-// ΨΔΣL345ΨΘL
 #include <bits/stdc++.h>
 using namespace std;
+
 #define nl "\n"
 #define pb push_back
 #define lsb(x) (x & -x) 
@@ -10,19 +10,6 @@ using namespace std;
 
 typedef long long ll;
 typedef unsigned long long ull;
-typedef pair<ll,ll>par;
-typedef vector<ll>vll;
-typedef vector<par>vpar;
-typedef vector<bool>vb;
-typedef vector<vll>vvll;
-typedef vector<vpar>vvpar;
-typedef vector<vb>vvb;
-
-/*'A' = 65 'Z' = 90
-  'a' = 97 'z' = 122
-  '0' = 48 '9' = 57 */
-
-ll n,m,q;
 
 int main(){
     cin.tie(0)->sync_with_stdio(0),cout.tie(0);
